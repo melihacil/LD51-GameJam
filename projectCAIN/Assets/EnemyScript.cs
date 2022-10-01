437b25d062de1a9c19968b190a71646dbf8d3a36
melihacil <knghtmellomello@gmail.com>
2022-10-01 15:26:34 +0300

    Updating

--- projectCAIN/Assets/EnemyScript.cs
+++ projectCAIN/Assets/EnemyScript.cs
@@ -4,6 +4,14 @@ using UnityEngine;
 
 public class EnemyScript : MonoBehaviour
 {
+
+
+    [SerializeField] private Transform playerPos;
+    [SerializeField] private float distanceBetween;
+    [SerializeField] private float speed;
+
+
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +21,7 @@ public class EnemyScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (Vector2.Distance(playerPos.position, transform.position) > distanceBetween)
+        transform.position = Vector2.MoveTowards(transform.position, playerPos.position, speed);
     }
 }
